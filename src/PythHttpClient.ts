5ef8c1b2fb500d25e2bebae0dd764a7269cf9a8b
Alfredo Di Maria <linuxloverstaff@gmail.com>
2021-11-12 22:52:12 +0100

    Indentation from 4 to 2 spaces

--- src/PythHttpClient.ts
+++ src/PythHttpClient.ts
@@ -1,20 +1,13 @@
-import { AccountInfo, Commitment, Connection, PublicKey } from "@solana/web3.js";
+import { Commitment, Connection, PublicKey } from "@solana/web3.js";
 import { Product, PriceData, parseProductData, parsePriceData, parseBaseData, AccountType } from ".";
 
-const ONES = '1111111111111111111111111111111';
-
-interface PriceRawData {
-    key: PublicKey;
-    account: AccountInfo<Buffer>;
-}
-
 export interface PythHttpClientResult {
-    assetTypes: string[];
-    symbols: string[];
-    products: Product[];
-    productFromSymbol: Map<string, Product>;
-    productPrice: Map<string, PriceData>;
-    prices: PriceData[];
+  assetTypes: string[];
+  symbols: string[];
+  products: Product[];
+  productFromSymbol: Map<string, Product>;
+  productPrice: Map<string, PriceData>;
+  prices: PriceData[];
 }
 
 /**
@@ -22,81 +15,81 @@ export interface PythHttpClientResult {
  * Use the method getData() to get updated prices values.
  */
 export class PythHttpClient {
-    connection: Connection;
-    pythProgramKey: PublicKey;
-    commitment: Commitment;
+  connection: Connection;
+  pythProgramKey: PublicKey;
+  commitment: Commitment;
 
-    constructor(connection: Connection, pythProgramKey: PublicKey, commitment: Commitment = 'finalized') {
-        this.connection = connection;
-        this.pythProgramKey = pythProgramKey;
-        this.commitment = commitment;
-    }
+  constructor(connection: Connection, pythProgramKey: PublicKey, commitment: Commitment = 'finalized') {
+    this.connection = connection;
+    this.pythProgramKey = pythProgramKey;
+    this.commitment = commitment;
+  }
 
-    /*
-    * Get Pyth Network account information and return actual price state.
-    * The result contains lists of asset types, product symbols and their prices.
-    */
-    public async getData(): Promise<PythHttpClientResult> {
-        const assetTypes = new Set<string>();
-        const productSymbols = new Set<string>();
-        const products = new Set<Product>()
-        const productFromSymbol = new Map<string, Product>()
-        const productPrice = new Map<string, PriceData>()
-        const prices = new Array<PriceData>();
+  /*
+  * Get Pyth Network account information and return actual price state.
+  * The result contains lists of asset types, product symbols and their prices.
+  */
+  public async getData(): Promise<PythHttpClientResult> {
+    const assetTypes = new Set<string>();
+    const productSymbols = new Set<string>();
+    const products = new Set<Product>()
+    const productFromSymbol = new Map<string, Product>()
+    const productPrice = new Map<string, PriceData>()
+    const prices = new Array<PriceData>();
 
-        // Retrieve data from blockchain
-        const accountList = await this.connection.getProgramAccounts(this.pythProgramKey, this.commitment);
-        
-        // Popolate producs and prices
-        const priceDataQueue = new Array<PriceData>();
-        const productAccountKeyToProduct = new Map<string, Product>();
+    // Retrieve data from blockchain
+    const accountList = await this.connection.getProgramAccounts(this.pythProgramKey, this.commitment);
 
-        accountList.forEach(singleAccount => {
-            const base = parseBaseData(singleAccount.account.data);
-            if(base) {
-                switch (AccountType[base.type]) {
-                    case 'Mapping':
-                        // We can skip these because we're going to get every account owned by this program anyway.
-                        break;
-                    case 'Product':
-                        const productData = parseProductData(singleAccount.account.data)
+    // Popolate producs and prices
+    const priceDataQueue = new Array<PriceData>();
+    const productAccountKeyToProduct = new Map<string, Product>();
 
-                        productAccountKeyToProduct.set(singleAccount.pubkey.toBase58(), productData.product)
-                        assetTypes.add(productData.product.asset_type);
-                        productSymbols.add(productData.product.symbol);
-                        products.add(productData.product);
-                        productFromSymbol.set(productData.product.symbol, productData.product);
-                        break;
-                    case 'Price':
-                        const priceData = parsePriceData(singleAccount.account.data)     
-                        priceDataQueue.push(priceData)
-                        break;
-                    case 'Test':
-                        break;
-                    default:
-                        throw new Error(`Unknown account type: ${base.type}. Try upgrading pyth-client.`)
-                }    
-            }
-        });
+    accountList.forEach(singleAccount => {
+      const base = parseBaseData(singleAccount.account.data);
+      if (base) {
+        switch (AccountType[base.type]) {
+          case 'Mapping':
+            // We can skip these because we're going to get every account owned by this program anyway.
+            break;
+          case 'Product':
+            const productData = parseProductData(singleAccount.account.data)
 
-        priceDataQueue.forEach(priceData => {
-            const product = productAccountKeyToProduct.get(priceData.productAccountKey.toBase58())
-            
-            if(product) {
-                productPrice.set(product.symbol, priceData);
-                prices.push(priceData);
-            }
-        });
+            productAccountKeyToProduct.set(singleAccount.pubkey.toBase58(), productData.product)
+            assetTypes.add(productData.product.asset_type);
+            productSymbols.add(productData.product.symbol);
+            products.add(productData.product);
+            productFromSymbol.set(productData.product.symbol, productData.product);
+            break;
+          case 'Price':
+            const priceData = parsePriceData(singleAccount.account.data)
+            priceDataQueue.push(priceData)
+            break;
+          case 'Test':
+            break;
+          default:
+            throw new Error(`Unknown account type: ${base.type}. Try upgrading pyth-client.`)
+        }
+      }
+    });
 
-        const result: PythHttpClientResult = {
-            assetTypes: Array.from(assetTypes),
-            symbols: Array.from(productSymbols),
-            products: Array.from(products),
-            productFromSymbol,
-            productPrice,
-            prices                   
-        };
+    priceDataQueue.forEach(priceData => {
+      const product = productAccountKeyToProduct.get(priceData.productAccountKey.toBase58())
 
-        return result;
-    }
+      if (product) {
+        productPrice.set(product.symbol, priceData);
+        prices.push(priceData);
+      }
+    });
+
+    const result: PythHttpClientResult = {
+      assetTypes: Array.from(assetTypes),
+      symbols: Array.from(productSymbols),
+      products: Array.from(products),
+      productFromSymbol,
+      productPrice,
+      prices
+    };
+
+    return result;
+  }
 }
--- src/__tests__/PythNetworkRestClient-test.ts
+++ src/__tests__/PythNetworkRestClient-test.ts
@@ -2,37 +2,37 @@ import { clusterApiUrl, Connection } from "@solana/web3.js";
 import { getPythProgramKeyForCluster, PythHttpClient } from "..";
 
 test('PythHttpClientCall', done => {
-    jest.setTimeout(20000)
-    try {
-        const programKey = getPythProgramKeyForCluster('testnet');
-        const currentConnection = new Connection(clusterApiUrl('testnet'));
-        const pyth_client = new PythHttpClient(currentConnection, programKey);
-        pyth_client.getData().then(
-            result => {
-                try {
-                    console.log("products number: ", result.products.length)
-                    console.log("asset types: ", result.assetTypes);
-                    console.log("product symbols: ", result.symbols);
+  jest.setTimeout(20000)
+  try {
+    const programKey = getPythProgramKeyForCluster('testnet');
+    const currentConnection = new Connection(clusterApiUrl('testnet'));
+    const pyth_client = new PythHttpClient(currentConnection, programKey);
+    pyth_client.getData().then(
+      result => {
+        try {
+          console.log("products number: ", result.products.length)
+          console.log("asset types: ", result.assetTypes);
+          console.log("product symbols: ", result.symbols);
 
-                    // Find a product with symbol "SOL/USD"
-                    const products = result.products.filter(p => p.symbol === "SOL/USD");
-                    expect(products.length).toBeGreaterThan(0);
-    
-                    // Find product prices
-                    const price = result.productPrice.get(products[0].symbol);
-                    expect(price).toBeDefined();
-    
-                    console.log("products", products)
-                    console.log("price", price)
-    
-                    done()    
-                } catch (cerr) {
-                    done(cerr)
-                }
-            }, 
-            err => done(err)
-        )
-    } catch(err_catch) {
-        done(err_catch);
-    }
+          // Find a product with symbol "SOL/USD"
+          const products = result.products.filter(p => p.symbol === "SOL/USD");
+          expect(products.length).toBeGreaterThan(0);
+
+          // Find product prices
+          const price = result.productPrice.get(products[0].symbol);
+          expect(price).toBeDefined();
+
+          console.log("products", products)
+          console.log("price", price)
+
+          done()
+        } catch (cerr) {
+          done(cerr)
+        }
+      },
+      err => done(err)
+    )
+  } catch (err_catch) {
+    done(err_catch);
+  }
 });
