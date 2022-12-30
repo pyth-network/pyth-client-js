6fb0434251db11cc53c0a1183a022a5ea6f07584
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-12-30 18:41:24 +0100

    Types

--- src/anchor/coder/types.ts
+++ src/anchor/coder/types.ts
@@ -2,9 +2,9 @@ import { Idl, TypesCoder } from '@coral-xyz/anchor'
 
 export class PythOracleTypesCoder implements TypesCoder {
   encode<T = any>(_name: string, _type: T): Buffer {
-    throw new Error('Pyth oracle does not have user-defined types')
+    throw new Error('Not implemented')
   }
   decode<T = any>(_name: string, _typeData: Buffer): T {
-    throw new Error('Pyth oracle does not have user-defined types')
+    throw new Error('Not implemented')
   }
 }
